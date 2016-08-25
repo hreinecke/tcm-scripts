3c47dabbf22115ff49230679ac47015ef5c58f52
Hannes Reinecke <hare@suse.de>
2016-08-25 14:58:02 +0200

    Add scripts for setting up FCoE

---
+++ remove_fileio.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+core=/sys/kernel/config/target/core
+
+for f in ${core}/fileio_* ; do
+    [ -d $f ] || continue
+    for d in ${f}/* ; do
+	[ -d $d ] || continue
+	for t in ${d}/alua/* ; do
+	    [ -d $t ] || continue
+	    [ ${t##*/} = "default_tg_pt_gp" ] && continue
+	    echo "Remove $t"
+	    rmdir $t || exit 1
+	done
+	echo "Remove $d"
+	rmdir $d || exit 1
+    done
+    echo "Remove $f"
+    rmdir $f || exit 1
+done
+
